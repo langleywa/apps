2b5f3a4eae4fec7c00cbca0686cd7c5b9ae12579
Aaron Racicot <aaronr@z-pulley.com>
2014-09-06 18:15:32 -0700

    Adding in an autocomplete for layers from the server to add to the layerlist

--- test/js/langley.js
+++ test/js/langley.js
@@ -1,4 +1,5 @@
 var map, layerControl, rsidebar, lsidebar, drawControl, drawnItems = null;
+var autocompdata = [];
 
 $(document).ready(function() {
 
@@ -20,36 +21,36 @@ $(document).ready(function() {
     // Test getting layers
     $.getJSON( "/gisdata/geojson/filelist.json").done(function( data ) {
         proj4defs = data;
-        var autocompdata = [];
         $.each( data, function( key, val ) {
-            for( var indx = 0; indx < 5; indx++ ){ //val.length; indx ++ ){
+            for( var indx = 0; indx < val.length; indx ++ ){
                 var filename = val[indx];
-                var featureLayer = L.mapbox.featureLayer()
-                    .loadURL('/gisdata/geojson/'+filename);
-                    //.addTo(map);
-                layerControl.addOverlay(featureLayer,filename);
+                autocompdata.push({label:filename,value:filename})
             }
-            autocompdata.push({label:key+'-'+val[0],value:key})
         });
-    //    $( "#projection" ).autocomplete({
-    //        source: autocompdata,
-    //        minLength: 3,
-    //        select: function( event, ui ) {
-    //            // Update all the proj windows
-    //            $('#projlabel').text('EPSG:'+ ui.item.value +' - ' + proj4defs[ui.item.value][0]);
-    //            currentproj = ui.item.value;
-    //            $('#boxboundsmerc').text(formatBounds(bounds.getBounds(),currentproj));
-    //            $('#mouseposmerc').text(formatPoint(new L.LatLng(0, 0),currentproj));
-    //            $('#mapboundsmerc').text(formatBounds(map.getBounds(),currentproj));
-    //            $('#centermerc').text(formatPoint(map.getCenter(),currentproj));
-    //        }
-    //    }).val('3857');
-    //    $('#projection').on( 'click', function(evt){
-    //         $( "#projection" ).autocomplete(  "search", currentproj  );
-    //    });        
-    //    // Set labels for output... left always 4326, right is proj selection
-    //    $('#wgslabel').text('EPSG:4326 - ' + proj4defs['4326'][0]);
-    //    $('#projlabel').text('EPSG:3857 - ' + proj4defs['3857'][0]);
+        $( "#projection" ).autocomplete({
+            source: autocompdata,
+            minLength: 0,
+            select: function( event, ui ) {
+                var filename = ui.item.value;
+                var featureLayer = L.mapbox.featureLayer()
+                    .loadURL('/gisdata/geojson/'+filename)
+                    .addTo(map);
+                layerControl.addOverlay(featureLayer,filename);
+                // Try to remove from autocomplete list
+                for( var indx = 0; indx < autocompdata.length; indx++ ){
+                    if (autocompdata[indx].value == filename) {
+                        var index = autocompdata.indexOf(filename);
+                        autocompdata.splice(indx, 1);
+                        $( "#projection" ).autocomplete( "option", "source", autocompdata);
+                        
+                    }
+                }
+                return false;
+            }
+        }).val('');
+        $('#projection').on( 'click', function(evt){
+             $( "#projection" ).autocomplete(  "search", "" );
+        });        
     }).fail(function( jqxhr, textStatus, error ) {
         var err = textStatus + ", " + error;
         console.log( "Request Failed: " + err );
